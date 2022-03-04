f01edea4533bb15108969ac604510a6f178850b8
HaJunRyu <hjr4557@naver.com>
2022-03-04 21:40:15 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js Test!</a>
         </h1>
 
         <p className={styles.description}>
